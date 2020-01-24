55442276c5146d83a5ac38d7cbea2d0370b0a57c
yomunsam <yomunsam@outlook.com>
2020-01-24 18:25:49 +0800

    🐱农历新年快乐~ 来自不能出去浪的咸鱼的提交

---
+++ Runtime/Scripts/Exceptions/XException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TinaX
+{
+    public class XException : Exception
+    {
+        public bool ServiceException { get; protected set; } = false;
+        public string ServiceName { get; protected set; } = "TinaX.Core";
+        public int ErrorCode { get; protected set; }
+        public XException(string msg) : base(msg) { }
+        public XException(string msg, int errorCode) : base(msg) { ErrorCode = errorCode; }
+        public XException (int errorCode) { ErrorCode = errorCode; }
+        public XException() { }
+
+    }
+}
+
