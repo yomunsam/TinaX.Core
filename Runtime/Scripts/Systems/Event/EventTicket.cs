2f1b08ec2f261205104798e7c68a4e1e8e44df91
yomunsam <yomunsam@outlook.com>
2020-03-30 01:02:23 +0800

    喵

---
+++ Runtime/Scripts/Systems/Event/EventTicket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinaX.Services
+{
+    public interface IEventTicket
+    {
+
+    }
+
+    public struct EventTicket : IEventTicket
+    {
+        public Action<object> handler;
+        public string group;
+        public string name;
+    }
+}
