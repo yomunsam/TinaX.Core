2c4c9efc7fe70edd58c21b6249510ce1caf2fff1
yomunsam <yomunsam@outlook.com>
2020-07-21 11:31:39 +0800

    ヾ(•ω•`)o 6.6.9

--- Runtime/Scripts/Utils/IO/XPath.cs
+++ Runtime/Scripts/Utils/IO/XPath.cs
@@ -35,6 +35,60 @@ namespace TinaX.IO
             return Directory.Exists(system_path);
         }
 
+        /// <summary>
+        /// 是否为子路径， 如果相等也返回false
+        /// </summary>
+        /// <param name="path1">路径1</param>
+        /// <param name="path2">路径2</param>
+        /// <param name="mutual">是否互相检测，如果为false，只检查path1是否为path2的子路径；如果为true，互相判断两者是否为对方的子路径</param>
+        /// <returns></returns>
+        public static bool IsSubpath(string path1, string path2, bool mutual = false)
+        {
+            string p1 = (path1.EndsWith("/") || path1.EndsWith("\\")) ? path1.Replace("\\", "/") : path1.Replace("\\", "/") + "/";
+            string p2 = (path2.EndsWith("/") || path2.EndsWith("\\")) ? path2.Replace("\\", "/") : path2.Replace("\\", "/") + "/";
+
+            if (p1 == p2) return false;
+
+            if (mutual)
+            {
+                //相互判断
+                return (p1.StartsWith(p2) || p2.StartsWith(p1));
+            }
+            else
+            {
+                //判断p1是否是p2的子路径
+                return p1.StartsWith(p2);
+            }
+
+        }
+
+
+        /// <summary>
+        /// 是否为子路径或相同路径
+        /// </summary>
+        /// <param name="path1">路径1</param>
+        /// <param name="path2">路径2</param>
+        /// <param name="mutual">是否互相检测，如果为false，只检查path1是否为path2的子路径；如果为true，互相判断两者是否为对方的子路径</param>
+        /// <returns></returns>
+        public static bool IsSameOrSubPath(string path1, string path2, bool mutual = false)
+        {
+            string p1 = (path1.EndsWith("/") || path1.EndsWith("\\")) ? path1.Replace("\\", "/") : path1.Replace("\\", "/") + "/";
+            string p2 = (path2.EndsWith("/") || path2.EndsWith("\\")) ? path2.Replace("\\", "/") : path2.Replace("\\", "/") + "/";
+
+            if (p1 == p2) return true;
+
+            if (mutual)
+            {
+                //相互判断
+                return (p1.StartsWith(p2) || p2.StartsWith(p1));
+            }
+            else
+            {
+                //判断p1是否是p2的子路径
+                return p1.StartsWith(p2);
+            }
+        }
+
 
     }
 }
