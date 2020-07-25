8fe695b89c7d91719d76c4be9c3b0fb555ea891a
yomunsam <yomunsam@outlook.com>
2020-07-25 09:35:28 +0800

    6.6.10 Support Unity 2020.1

--- Editor/Vender/ToolbarCallback.cs
+++ Editor/Vender/ToolbarCallback.cs
@@ -15,8 +15,16 @@ namespace UnityToolbarExtender
 	{
 		static Type m_toolbarType = typeof(Editor).Assembly.GetType("UnityEditor.Toolbar");
 		static Type m_guiViewType = typeof(Editor).Assembly.GetType("UnityEditor.GUIView");
+#if UNITY_2020_1_OR_NEWER
+		static Type m_iWindowBackendType = typeof(Editor).Assembly.GetType("UnityEditor.IWindowBackend");
+		static PropertyInfo m_windowBackend = m_guiViewType.GetProperty("windowBackend",
+			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+		static PropertyInfo m_viewVisualTree = m_iWindowBackendType.GetProperty("visualTree",
+			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+#else
 		static PropertyInfo m_viewVisualTree = m_guiViewType.GetProperty("visualTree",
 			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+#endif
 		static FieldInfo m_imguiContainerOnGui = typeof(IMGUIContainer).GetField("m_OnGUIHandler",
 			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 		static ScriptableObject m_currentToolbar;
@@ -39,17 +47,24 @@ namespace UnityToolbarExtender
 			{
 				// Find toolbar
 				var toolbars = Resources.FindObjectsOfTypeAll(m_toolbarType);
-				m_currentToolbar = toolbars.Length > 0 ? (ScriptableObject) toolbars[0] : null;
+				m_currentToolbar = toolbars.Length > 0 ? (ScriptableObject)toolbars[0] : null;
 				if (m_currentToolbar != null)
 				{
+#if UNITY_2020_1_OR_NEWER
+					var windowBackend = m_windowBackend.GetValue(m_currentToolbar);
+
 					// Get it's visual tree
-					var visualTree = (VisualElement) m_viewVisualTree.GetValue(m_currentToolbar, null);
+					var visualTree = (VisualElement) m_viewVisualTree.GetValue(windowBackend, null);
+#else
+					// Get it's visual tree
+					var visualTree = (VisualElement)m_viewVisualTree.GetValue(m_currentToolbar, null);
+#endif
 
 					// Get first child which 'happens' to be toolbar IMGUIContainer
-					var container = (IMGUIContainer) visualTree[0];
+					var container = (IMGUIContainer)visualTree[0];
 
 					// (Re)attach handler
-					var handler = (Action) m_imguiContainerOnGui.GetValue(container);
+					var handler = (Action)m_imguiContainerOnGui.GetValue(container);
 					handler -= OnGUI;
 					handler += OnGUI;
 					m_imguiContainerOnGui.SetValue(container, handler);
--- Editor/Vender/ToolbarExtender.cs
+++ Editor/Vender/ToolbarExtender.cs
@@ -18,27 +18,43 @@ namespace UnityToolbarExtender
 		static ToolbarExtender()
 		{
 			Type toolbarType = typeof(Editor).Assembly.GetType("UnityEditor.Toolbar");
-			
+
 #if UNITY_2019_1_OR_NEWER
 			string fieldName = "k_ToolCount";
 #else
 			string fieldName = "s_ShownToolIcons";
 #endif
-			
+
 			FieldInfo toolIcons = toolbarType.GetField(fieldName,
 				BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
-			
-#if UNITY_2019_1_OR_NEWER
+
+#if UNITY_2019_3_OR_NEWER
+			m_toolCount = toolIcons != null ? ((int)toolIcons.GetValue(null)) : 8;
+#elif UNITY_2019_1_OR_NEWER
 			m_toolCount = toolIcons != null ? ((int) toolIcons.GetValue(null)) : 7;
 #elif UNITY_2018_1_OR_NEWER
 			m_toolCount = toolIcons != null ? ((Array) toolIcons.GetValue(null)).Length : 6;
 #else
 			m_toolCount = toolIcons != null ? ((Array) toolIcons.GetValue(null)).Length : 5;
 #endif
-	
+
 			ToolbarCallback.OnToolbarGUI -= OnGUI;
 			ToolbarCallback.OnToolbarGUI += OnGUI;
 		}
+
+#if UNITY_2019_3_OR_NEWER
+		public const float space = 8;
+#else
+		public const float space = 10;
+#endif
+		public const float largeSpace = 20;
+		public const float buttonWidth = 32;
+		public const float dropdownWidth = 80;
+#if UNITY_2019_1_OR_NEWER
+		public const float playPauseStopWidth = 140;
+#else
+		public const float playPauseStopWidth = 100;
+#endif
 
 		static void OnGUI()
 		{
@@ -53,12 +69,16 @@ namespace UnityToolbarExtender
 			var screenWidth = EditorGUIUtility.currentViewWidth;
 
 			// Following calculations match code reflected from Toolbar.OldOnGUI()
-			float playButtonsPosition = (screenWidth - 100) / 2;
+			float playButtonsPosition = Mathf.RoundToInt((screenWidth - playPauseStopWidth) / 2);
 
 			Rect leftRect = new Rect(0, 0, screenWidth, Screen.height);
-			leftRect.xMin += 10; // Spacing left
-			leftRect.xMin += 32 * m_toolCount; // Tool buttons
-			leftRect.xMin += 20; // Spacing between tools and pivot
+			leftRect.xMin += space; // Spacing left
+			leftRect.xMin += buttonWidth * m_toolCount; // Tool buttons
+#if UNITY_2019_3_OR_NEWER
+			leftRect.xMin += space; // Spacing between tools and pivot
+#else
+			leftRect.xMin += largeSpace; // Spacing between tools and pivot
+#endif
 			leftRect.xMin += 64 * 2; // Pivot buttons
 			leftRect.xMax = playButtonsPosition;
 
@@ -66,28 +86,39 @@ namespace UnityToolbarExtender
 			rightRect.xMin = playButtonsPosition;
 			rightRect.xMin += m_commandStyle.fixedWidth * 3; // Play buttons
 			rightRect.xMax = screenWidth;
-			rightRect.xMax -= 10; // Spacing right
-			rightRect.xMax -= 80; // Layout
-			rightRect.xMax -= 10; // Spacing between layout and layers
-			rightRect.xMax -= 80; // Layers
-			rightRect.xMax -= 20; // Spacing between layers and account
-			rightRect.xMax -= 80; // Account
-			rightRect.xMax -= 10; // Spacing between account and cloud
-			rightRect.xMax -= 32; // Cloud
-			rightRect.xMax -= 10; // Spacing between cloud and collab
+			rightRect.xMax -= space; // Spacing right
+			rightRect.xMax -= dropdownWidth; // Layout
+			rightRect.xMax -= space; // Spacing between layout and layers
+			rightRect.xMax -= dropdownWidth; // Layers
+#if UNITY_2019_3_OR_NEWER
+			rightRect.xMax -= space; // Spacing between layers and account
+#else
+			rightRect.xMax -= largeSpace; // Spacing between layers and account
+#endif
+			rightRect.xMax -= dropdownWidth; // Account
+			rightRect.xMax -= space; // Spacing between account and cloud
+			rightRect.xMax -= buttonWidth; // Cloud
+			rightRect.xMax -= space; // Spacing between cloud and collab
 			rightRect.xMax -= 78; // Colab
 
 			// Add spacing around existing controls
-			leftRect.xMin += 10;
-			leftRect.xMax -= 10;
-			rightRect.xMin += 10;
-			rightRect.xMax -= 10;
+			leftRect.xMin += space;
+			leftRect.xMax -= space;
+			rightRect.xMin += space;
+			rightRect.xMax -= space;
 
 			// Add top and bottom margins
+#if UNITY_2019_3_OR_NEWER
+			leftRect.y = 4;
+			leftRect.height = 22;
+			rightRect.y = 4;
+			rightRect.height = 22;
+#else
 			leftRect.y = 5;
 			leftRect.height = 24;
 			rightRect.y = 5;
 			rightRect.height = 24;
+#endif
 
 			if (leftRect.width > 0)
 			{
